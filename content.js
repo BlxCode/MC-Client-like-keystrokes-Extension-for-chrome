623b60d87c41b2cd106416c07fdca6fc538e71fb
BlxCode <bloxdmasterbloxdacc@gmail.com>
2025-07-10 10:11:19 -0700

    e

--- content.js
+++ content.js
@@ -1,22 +1,83 @@
 
+var cpsNum = 0;
+var secsPassed = 0;
+var bgColorSelected;
+var bgColor;
+var Color;
+var ColorSelected;
+var size;
+chrome.storage.local.get(
+  ["bgColorSelected", "bgColor", "Color", "ColorSelected","size"],
+  function (result) {
+    bgColorSelected = result.bgColorSelected || bgColorSelected;
+    console.log(bgColorSelected + " BGCOLORSOLECTED");
+    bgColor = result.bgColor || bgColor;
+    console.log(bgColor + " BG COLOR");
+    console.log(Color + "COLOR");
 
-
+    Color = result.Color || Color;
+size = result.Size || size;
+    ColorSelected = result.ColorSelected || ColorSelected;
+    if (bgColorSelected == undefined && bgColor == undefined) {
+      bgColorSelected = "#ffffff";
+      bgColor = "#909090";
+      Color = "#000000";
+      ColorSelected = "#000000";
+      size = 1;
+      chrome.storage.local.set({
+        bgColorSelected: bgColorSelected,
+        bgColor: bgColor,
+        Color: Color,
+        ColorSelected: ColorSelected,
+        size:size
+      });
+    }
+    w.style.backgroundColor = bgColor;
+    a.style.backgroundColor = bgColor;
+    s.style.backgroundColor = bgColor;
+    d.style.backgroundColor = bgColor;
+    space.style.backgroundColor = bgColor;
+    shift.style.backgroundColor = bgColor;
+    rmb.style.backgroundColor = bgColor;
+    lmb.style.backgroundColor = bgColor;
+    cps.style.backgroundColor = bgColor;
+    cps.style.color = Color;
+    w.style.color = Color;
+    a.style.color = Color;
+    s.style.color = Color;
+    d.style.color = Color;
+    space.style.color = Color;
+    shift.style.color = Color;
+    rmb.style.color = Color;
+    lmb.style.color = Color;
+  }
+);
 const link = document.createElement("link");
-link.href = "https://fonts.googleapis.com/css2?family=Fredoka:wght@300..700&display=swap";
+link.href =
+  "https://fonts.googleapis.com/css2?family=Fredoka:wght@300..700&display=swap";
 link.rel = "stylesheet";
 document.head.appendChild(link);
 // Load Settings
-function makeSpace (wat) {
-document.body.appendChild(wat);
+function makeSpace(wat) {
+  KeystrokeWrapper.appendChild(wat);
 }
-var KeystrokeWrapper = document.createElement("blxms-keystroke-web-module-cr-ext");
+var KeystrokeWrapper = document.createElement(
+  "blxms-keystroke-web-module-cr-ext"
+);
 KeystrokeWrapper.id = "KeystrokeWrapper-CrExt";
 document.body.appendChild(KeystrokeWrapper);
 var LMB = document.createElement("div");
-function makeStroke (variable, text){
-variable.innerText = text;
-variable.className = "Keystroke-CrExt";
-KeystrokeWrapper.appendChild(variable);
+function makeStroke(variable, text) {
+  variable.innerText = text;
+  variable.className = "Keystroke-CrExt";
+  KeystrokeWrapper.appendChild(variable);
+  if(size != 1){
+   
+    variable.style.width=3*size+"em".toString();
+   
+    variable.style.height=3*size+"em".toString();
+    variable.style.fontSize=2.5*size+"em".toString();
+  }
 }
 var cps = document.createElement("div");
 makeStroke(cps, "CPS: 0");
@@ -43,119 +104,85 @@ var space = document.createElement("div");
 makeStroke(space, "Space");
 var shift = document.createElement("div");
 makeStroke(shift, "Shift");
-chrome.storage.local.get(['bgColorSelected', 'bgColor', 'Color', 'ColorSelected'], function(result) {
-    bgColorSelected = result.bgColorSelected || bgColorSelected;
-    console.log(bgColorSelected+" BGCOLORSOLECTED");
-    bgColor = result.bgColor || bgColor;
-    console.log(bgColor+" BG COLOR");
-    console.log(Color + "COLOR");
-    
-    Color = result.Color || Color;
-    
-    ColorSelected = result.ColorSelected || ColorSelected;
-if(bgColorSelected == undefined && bgColor == undefined){
-bgColorSelected = "#ffffff";
-bgColor = "#909090";
-Color = "#000000";
-ColorSelected = "#000000";
- chrome.storage.local.set({bgColorSelected: bgColorSelected, bgColor: bgColor, Color: Color, ColorSelected: ColorSelected});
 
-}
-    w.style.backgroundColor = bgColor;
-    a.style.backgroundColor = bgColor;
-    s.style.backgroundColor = bgColor;
-    d.style.backgroundColor = bgColor;
-    space.style.backgroundColor = bgColor;
-    shift.style.backgroundColor = bgColor;
-    rmb.style.backgroundColor = bgColor;
-    lmb.style.backgroundColor = bgColor;
-    cps.style.backgroundColor = bgColor;
-    cps.style.color = Color;
-    w.style.color = Color;
-    a.style.color = Color;
-    s.style.color = Color;
-    d.style.color = Color;
-    space.style.color = Color;
-    shift.style.color = Color;
-    rmb.style.color = Color;
-    lmb.style.color = Color;
-});
 
-var cpsNum = 0;
-var secsPassed = 0;
-var bgColorSelected;
-var bgColor;
-var Color;
-var ColorSelected;
 
-document.addEventListener("keydown", (event)=>{
-if(event.key=="w"){
+document.addEventListener("keydown", (event) => {
+  if (event.key == "w") {
     w.style.backgroundColor = bgColorSelected;
-} if(event.key=="a"){
+  }
+  if (event.key == "a") {
     a.style.backgroundColor = bgColorSelected;
-} if(event.key=="s"){
+  }
+  if (event.key == "s") {
     s.style.backgroundColor = bgColorSelected;
-} if(event.key=="d"){
+  }
+  if (event.key == "d") {
     d.style.backgroundColor = bgColorSelected;
-} if(event.key==" "){
+  }
+  if (event.key == " ") {
     space.style.backgroundColor = bgColorSelected;
-} if(event.key=="Shift"){
+  }
+  if (event.key == "Shift") {
     shift.style.backgroundColor = bgColorSelected;
-}
+  }
 });
-document.addEventListener("keyup", (event)=>{
-if(event.key=="w"){
+document.addEventListener("keyup", (event) => {
+  if (event.key == "w") {
     w.style.backgroundColor = bgColor;
-}
-if(event.key=="a"){
+  }
+  if (event.key == "a") {
     a.style.backgroundColor = bgColor;
-}
-if(event.key=="s"){
+  }
+  if (event.key == "s") {
     s.style.backgroundColor = bgColor;
-}
-if(event.key=="d"){
+  }
+  if (event.key == "d") {
     d.style.backgroundColor = bgColor;
-}
-if(event.key==" "){
+  }
+  if (event.key == " ") {
     space.style.backgroundColor = bgColor;
-}
-if(event.key=="Shift"){
+  }
+  if (event.key == "Shift") {
     shift.style.backgroundColor = bgColor;
-}
+  }
 });
-document.addEventListener("click", ()=>{cpsNum++;});
-document.addEventListener("mousedown", function(event) {
-    cpsNum++;
-    if (event.button === 0) { // Left mouse button
+document.addEventListener("click", () => {
+  cpsNum++;
+});
+document.addEventListener("mousedown", function (event) {
+  cpsNum++;
+  if (event.button === 0) {
+    // Left mouse button
     lmb.style.backgroundColor = bgColorSelected;
-
-    } else if (event.button === 2) { // Right mouse button
-      rmb.style.backgroundColor = bgColorSelected;
-    }
+  } else if (event.button === 2) {
+    // Right mouse button
+    rmb.style.backgroundColor = bgColorSelected;
+  }
 });
-document.addEventListener("mouseup", function(event) {
-    cpsNum++;
-    if (event.button === 0) { // Left mouse button
+document.addEventListener("mouseup", function (event) {
+  cpsNum++;
+  if (event.button === 0) {
+    // Left mouse button
     lmb.style.backgroundColor = bgColor;
-
-    } else if (event.button === 2) { // Right mouse button
-      rmb.style.backgroundColor = bgColor;
-    }
+  } else if (event.button === 2) {
+    // Right mouse button
+    rmb.style.backgroundColor = bgColor;
+  }
 });
 let clickTimestamps = [];
 
 document.addEventListener("click", () => {
-    clickTimestamps.push(Date.now());
-        const now = Date.now();
-    clickTimestamps = clickTimestamps.filter(ts => now - ts <= 1000);
-    const cpsNum = clickTimestamps.length;
-    cps.innerText = "CPS: " + cpsNum;
+  clickTimestamps.push(Date.now());
+  const now = Date.now();
+  clickTimestamps = clickTimestamps.filter((ts) => now - ts <= 1000);
+  const cpsNum = clickTimestamps.length;
+  cps.innerText = "CPS: " + cpsNum;
 });
 
 setInterval(() => {
-    const now = Date.now();
-    clickTimestamps = clickTimestamps.filter(ts => now - ts <= 1000);
-    const cpsNum = clickTimestamps.length;
-    cps.innerText = "CPS: " + cpsNum;
+  const now = Date.now();
+  clickTimestamps = clickTimestamps.filter((ts) => now - ts <= 1000);
+  const cpsNum = clickTimestamps.length;
+  cps.innerText = "CPS: " + cpsNum;
 }, 500);
-
--- popup.js
+++ popup.js
@@ -1,4 +1,3 @@
-
 var cps = document.getElementById("cps");
 var rmb = document.getElementById("rmb");
 var w = document.getElementById("w");
@@ -11,39 +10,50 @@ var shift = document.getElementById("shift");
 var c = document.getElementById("c");
 var cpsNum = 0;
 var secsPassed = 0;
-var bgColorSelected = document.getElementById("Keystrokes-Selected-Background-Color").value;
+var bgColorSelected = document.getElementById(
+  "Keystrokes-Selected-Background-Color"
+).value;
 var bgColor = document.getElementById("Keystrokes-Background-Color").value;
 var Color = document.getElementById("Keystrokes-Color").value;
 var ColorSelected = document.getElementById("Keystrokes-Selected-Color").value;
 var size = document.getElementById("Keystrokes-Size").value;
-document.getElementById("apply").addEventListener("click", function() {
-     bgColorSelected = document.getElementById("Keystrokes-Selected-Background-Color").value;
-     size = document.getElementById("Keystrokes-Size").value;
- bgColor = document.getElementById("Keystrokes-Background-Color").value;
- Color = document.getElementById("Keystrokes-Color").value;
- ColorSelected = document.getElementById("Keystrokes-Selected-Color").value;
-    chrome.storage.local.set({bgColorSelected: bgColorSelected, bgColor: bgColor, Color: Color, ColorSelected: ColorSelected,size:size});
-    w.style.backgroundColor = bgColor;
-    a.style.backgroundColor = bgColor;
-    s.style.backgroundColor = bgColor;
-    d.style.backgroundColor = bgColor;
-    space.style.backgroundColor = bgColor;
-    shift.style.backgroundColor = bgColor;
-    rmb.style.backgroundColor = bgColor;
-    lmb.style.backgroundColor = bgColor;
-    cps.style.backgroundColor = bgColor;
-    cps.style.color = Color;
-    w.style.color = Color;
-    a.style.color = Color;
-    s.style.color = Color;
-    d.style.color = Color;
-    space.style.color = Color;
-    shift.style.color = Color;
-    rmb.style.color = Color;
-    lmb.style.color = Color;
-
+document.getElementById("apply").addEventListener("click", function () {
+  bgColorSelected = document.getElementById(
+    "Keystrokes-Selected-Background-Color"
+  ).value;
+  size = document.getElementById("Keystrokes-Size").value;
+  bgColor = document.getElementById("Keystrokes-Background-Color").value;
+  Color = document.getElementById("Keystrokes-Color").value;
+  ColorSelected = document.getElementById("Keystrokes-Selected-Color").value;
+  chrome.storage.local.set({
+    bgColorSelected: bgColorSelected,
+    bgColor: bgColor,
+    Color: Color,
+    ColorSelected: ColorSelected,
+    size: size,
+  });
+  w.style.backgroundColor = bgColor;
+  a.style.backgroundColor = bgColor;
+  s.style.backgroundColor = bgColor;
+  d.style.backgroundColor = bgColor;
+  space.style.backgroundColor = bgColor;
+  shift.style.backgroundColor = bgColor;
+  rmb.style.backgroundColor = bgColor;
+  lmb.style.backgroundColor = bgColor;
+  cps.style.backgroundColor = bgColor;
+  cps.style.color = Color;
+  w.style.color = Color;
+  a.style.color = Color;
+  s.style.color = Color;
+  d.style.color = Color;
+  space.style.color = Color;
+  shift.style.color = Color;
+  rmb.style.color = Color;
+  lmb.style.color = Color;
 });
-chrome.storage.local.get(['bgColorSelected', 'bgColor', 'Color', 'ColorSelected','size'], function(result) {
+chrome.storage.local.get(
+  ["bgColorSelected", "bgColor", "Color", "ColorSelected", "size"],
+  function (result) {
     bgColorSelected = result.bgColorSelected || bgColorSelected;
     bgColor = result.bgColor || bgColor;
     Color = result.Color || Color;
@@ -70,92 +80,101 @@ chrome.storage.local.get(['bgColorSelected', 'bgColor', 'Color', 'ColorSelected'
     shift.style.color = Color;
     rmb.style.color = Color;
     lmb.style.color = Color;
-    document.getElementById("Keystrokes-Size-Text").innerText = "Keystrokes Size: "+size;
+    document.getElementById("Keystrokes-Size-Text").innerText =
+      "Keystrokes Size: " + size;
     document.getElementById("Keystrokes-Size").value = size;
-    document.getElementById("Keystrokes-Selected-Background-Color").value = bgColorSelected;
+    document.getElementById("Keystrokes-Selected-Background-Color").value =
+      bgColorSelected;
     document.getElementById("Keystrokes-Background-Color").value = bgColor;
     document.getElementById("Keystrokes-Color").value = Color;
     document.getElementById("Keystrokes-Selected-Color").value = ColorSelected;
-});
-document.addEventListener("keydown", (event)=>{
-if(event.key=="w"){
+  }
+);
+document.addEventListener("keydown", (event) => {
+  if (event.key == "w") {
     w.style.backgroundColor = bgColorSelected;
-} if(event.key=="a"){
+  }
+  if (event.key == "a") {
     a.style.backgroundColor = bgColorSelected;
-} if(event.key=="s"){
+  }
+  if (event.key == "s") {
     s.style.backgroundColor = bgColorSelected;
-} if(event.key=="d"){
+  }
+  if (event.key == "d") {
     d.style.backgroundColor = bgColorSelected;
-} if(event.key==" "){
+  }
+  if (event.key == " ") {
     space.style.backgroundColor = bgColorSelected;
-} if(event.key=="Shift"){
+  }
+  if (event.key == "Shift") {
     shift.style.backgroundColor = bgColorSelected;
-} if(event.key=="c"){
+  }
+  if (event.key == "c") {
     c.style.backgroundColor = bgColorSelected;
-}
+  }
 });
-document.addEventListener("keyup", (event)=>{
-if(event.key=="w"){
+document.addEventListener("keyup", (event) => {
+  if (event.key == "w") {
     w.style.backgroundColor = bgColor;
-}
-if(event.key=="c"){
+  }
+  if (event.key == "c") {
     c.style.backgroundColor = bgColor;
-}
-if(event.key=="a"){
+  }
+  if (event.key == "a") {
     a.style.backgroundColor = bgColor;
-}
-if(event.key=="s"){
+  }
+  if (event.key == "s") {
     s.style.backgroundColor = bgColor;
-}
-if(event.key=="d"){
+  }
+  if (event.key == "d") {
     d.style.backgroundColor = bgColor;
-}
-if(event.key==" "){
+  }
+  if (event.key == " ") {
     space.style.backgroundColor = bgColor;
-}
-if(event.key=="Shift"){
+  }
+  if (event.key == "Shift") {
     shift.style.backgroundColor = bgColor;
-}
+  }
 });
-document.addEventListener("click", ()=>{cpsNum++;});
-document.addEventListener("mousedown", function(event) {
-    cpsNum++;
-    if (event.button === 0) { // Left mouse button
+document.addEventListener("click", () => {
+  cpsNum++;
+});
+document.addEventListener("mousedown", function (event) {
+  cpsNum++;
+  if (event.button === 0) {
+    // Left mouse button
     lmb.style.backgroundColor = bgColorSelected;
-
-    } else if (event.button === 2) { // Right mouse button
-      rmb.style.backgroundColor = bgColorSelected;
-    }
+  } else if (event.button === 2) {
+    // Right mouse button
+    rmb.style.backgroundColor = bgColorSelected;
+  }
 });
-document.addEventListener("mouseup", function(event) {
-    cpsNum++;
-    if (event.button === 0) { // Left mouse button
+document.addEventListener("mouseup", function (event) {
+  cpsNum++;
+  if (event.button === 0) {
+    // Left mouse button
     lmb.style.backgroundColor = bgColor;
-
-    } else if (event.button === 2) { // Right mouse button
-      rmb.style.backgroundColor = bgColor;
-    }
+  } else if (event.button === 2) {
+    // Right mouse button
+    rmb.style.backgroundColor = bgColor;
+  }
 });
 let clickTimestamps = [];
 
 document.addEventListener("click", () => {
-    clickTimestamps.push(Date.now());
+  clickTimestamps.push(Date.now());
 });
 
 setInterval(() => {
-    const now = Date.now();
-    clickTimestamps = clickTimestamps.filter(ts => now - ts <= 1000);
-    const cpsNum = clickTimestamps.length;
-    cps.innerText = "CPS: " + cpsNum;
+  const now = Date.now();
+  clickTimestamps = clickTimestamps.filter((ts) => now - ts <= 1000);
+  const cpsNum = clickTimestamps.length;
+  cps.innerText = "CPS: " + cpsNum;
 }, 250);
 
-
-
-document.getElementById("Keystrokes-Size").addEventListener("change", function() {
-    document.getElementById("Keystrokes-Size-Text").innerText = "Keystrokes Size: "+this.value;
-  
-    document.querySelectorAll(".Keystroke-CrExt").forEach(function(element) {
-        element.style.fontSize = this.value + "px";
-    });
-});
-
+document
+  .getElementById("Keystrokes-Size")
+  .addEventListener("change", function () {
+    document.getElementById("Keystrokes-Size-Text").innerText =
+      "Keystrokes Size: " + this.value;
+  });
